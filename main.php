0111f45e0535b8883fbd2fbfd6193f5502c7b080
TanakaKazune <t.kazune.cerezo@gmail.com>
2021-05-17 03:41:28 +0900

    クラスの定義

---
+++ main.php
@@ -0,0 +1,12 @@
+<div class="header">
+  <p>麻雀成績管理サイト</p>
+ <div class="header-list">
+  <ul>
+   <li><a href="">対局</a></li>
+   <li><a href="">戦績</a></li>
+   <li><a href="">会員登録</a></li>
+   <li><a href="">ログイン</a></li>
+   <li><a href="">ログアウト</a></li>
+  </ul>
+ </div>
+</div> 
