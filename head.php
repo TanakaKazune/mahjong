25c718068cd106923da3608a570e419e313755cd
TanakaKazune <t.kazune.cerezo@gmail.com>
2021-05-17 03:42:00 +0900

    cssファイルの読み込み

--- head.php
+++ head.php
@@ -8,4 +8,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=mahjong_date;charser=utf8','staff','
 <head>
 <meta charset="UTF-8">
 <title>麻雀成績管理サイト</title>
+<link rel="stylesheet" href="stylesheet.css">
 <body>
+
+<?php require_once('main.php') ?>
