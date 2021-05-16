21da0b7948a127eaf6905d92b7153203978f5a52
TanakaKazune <t.kazune.cerezo@gmail.com>
2021-05-17 02:46:48 +0900

    headerとfooterの作成

---
+++ head.php
@@ -0,0 +1,10 @@
+<? php
+$pdo = new PDO('mysql:host=localhost;dbname=mahjong_date;charser=utf8','staff','password');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>麻雀成績管理サイト</title>
+<body>
