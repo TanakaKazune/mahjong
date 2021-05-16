98cf3f718f0a60f75945bde6c0f10a76923d01d4
TanakaKazune <t.kazune.cerezo@gmail.com>
2021-05-17 05:12:24 +0900

    login.phpの作成

---
+++ login/login.php
@@ -0,0 +1,12 @@
+<?php require_once '../head.php' ?>
+
+<div class="login-input">
+ <form action="login-output.php" method="post">
+ ログインID<input type="text" name="id"><br>
+ 氏名      <input type="text" name="name"><br>
+ パスワード<input type="password" name="password"><br>
+ <input type="submit" value="ログイン">
+ </form>
+</div>
+
+<?php require_once '../foot.php' ?>
