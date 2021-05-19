731ac79978acb58f9bd949b55156c747a826b97d
TanakaKazune <t.kazune.cerezo@gmail.com>
2021-05-19 16:02:13 +0900

    相対パスの追加

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
   <p class="tittle">麻雀成績管理サイト</p>
  <div class="header-list">
   <ul>
-   <li><a href="">対局</a></li>
+   <li><a href="../game/game.php">対局</a></li>
    <li><a href="">戦績</a></li>
    <li><a href="../registration/registration.php">会員登録</a></li>
    <li><a href="../login/login.php">ログイン</a></li>
