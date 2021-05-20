2e761482275b0b47f110f2fcdc2d87caba339afe
TanakaKazune <t.kazune.cerezo@gmail.com>
2021-05-21 00:56:28 +0900

    相対パスの追加

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
  <div class="header-list">
   <ul>
    <li><a href="../game/game.php">対局</a></li>
-   <li><a href="">戦績</a></li>
+   <li><a href="../result/result.php">戦績</a></li>
    <li><a href="../registration/registration.php">会員登録</a></li>
    <li><a href="../login/login.php">ログイン</a></li>
    <li><a href="../logout/logout.php">ログアウト</a></li>
