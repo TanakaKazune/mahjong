029eb1cb1ebafaf6e36d8c0937c98c179a4e47e3
TanakaKazune <t.kazune.cerezo@gmail.com>
2021-05-22 18:06:16 +0900

    リロードすると複数回データベースへインサートするバグを修正

--- game/game-set.php
+++ game/game-set.php
@@ -43,7 +43,7 @@
     echo "参加するユーザー　",$row['user1'];
     echo "、",$row['user2'];
     echo "、",$row['user3'];
-    if($row['people'] = 4){
+    if($row['people'] == 4){
         echo "、",$row['user4'];
     }
   }
@@ -53,12 +53,17 @@
 <?php break ?>
 <?php case 'create': ?>
  <?php
-   if(!isset($_REQUEST['id4'])){
-     $_REQUEST['id4'] = 'null';
-   }
-   $sql = $pdo->prepare('insert into battle_management values(null,?,?,?,?,?,?,?,?)');
-   $sql->execute([$_REQUEST['date'],$_REQUEST['point-rate'],$_REQUEST['tip-rate'],$_REQUEST['people'],
-   $_REQUEST['id1'],$_REQUEST['id2'],$_REQUEST['id3'],$_REQUEST['id4']]);
+   if(!isset($_SESSION['user']['game'])){
+     if($_REQUEST['people'] = 4){
+      $sql = $pdo->prepare('insert into battle_management values(null,?,?,?,?,?,?,?,?)');
+      $sql->execute([$_REQUEST['date'],$_REQUEST['point-rate'],$_REQUEST['tip-rate'],$_REQUEST['people'],
+      $_REQUEST['id1'],$_REQUEST['id2'],$_REQUEST['id3'],$_REQUEST['id4']]);
+     }else{
+      $sql = $pdo->prepare('insert into battle_management values(null,?,?,?,?,?,?,?,null)');
+      $sql->execute([$_REQUEST['date'],$_REQUEST['point-rate'],$_REQUEST['tip-rate'],$_REQUEST['people'],
+      $_REQUEST['id1'],$_REQUEST['id2'],$_REQUEST['id3']]);
+     }
+    }
 
    $sql = $pdo->prepare('select battle_management.id as id, point_rate, tip_rate, people,
    user1.name as user1, user2.name as user2, user3.name as user3, user4.name as user4
@@ -81,23 +86,19 @@
     echo "参加するユーザー　",$row['user1'];
     echo "、",$row['user2'];
     echo "、",$row['user3'];
-    if($row['people'] = 4){
+    if($row['people'] == 4){
         echo "、",$row['user4'];
     }
 
     if(isset($_SESSION['user'])){
-      foreach($sql as $row){
-        $_SESSION['user'] = [
+       $_SESSION['user'] = [
           'id' => $_SESSION['user']['id'],
           'name' => $_SESSION['user']['name'],
           'game' => $row['id']];
-      }
     }else{
-      foreach($sql as $row){
         $_SESSION['user'] = ['game' => $row['id']];
-      }
     }
-   } 
+   }
  ?>
  <br>
  <a href="game.php">ゲーム開始</a>
